838c5f4f1861e783cd7538502688e1b76a681635
DucJS <mrtranduc1994@outlook.com>
2020-08-25 00:52:35 +0700

    create login action

--- src/actions/auth.action.js
+++ src/actions/auth.action.js
@@ -3,7 +3,11 @@ import { authConstants } from '../constants'
 const {
     REGISTER_REQUEST,
     REGISTER_SUCCESS,
-    REGISTER_FAIL
+    REGISTER_FAIL,
+
+    LOGIN_REQUEST,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL
 } = authConstants
 
 export function registerRequest(data) {
@@ -23,6 +27,27 @@ export function registerSuccess(data) {
 export function registerFail(data) {
     return {
         type: REGISTER_FAIL,
+        payload: data
+    }
+}
+
+export function loginRequest(data) {
+    return {
+        type: LOGIN_REQUEST,
+        payload: data
+    }
+}
+
+export function loginSuccess(data) {
+    return {
+        type: LOGIN_SUCCESS,
+        payload: data
+    }
+}
+
+export function loginFail(data) {
+    return {
+        type: LOGIN_FAIL,
         payload: data
     }
 }
