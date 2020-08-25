a22273b2197b4236b2969035e3e1a2cd5012c43b
DucJS <mrtranduc1994@outlook.com>
2020-08-25 23:35:58 +0700

    repair use makeStyles

--- src/style/UserProfile.style.js
+++ src/style/UserProfile.style.js
@@ -1,7 +1,7 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export const UserProfileUseStyles = {
+export const UserProfileUseStyles = makeStyles({
     root: {
         display: "none"
     }
-}
+})
