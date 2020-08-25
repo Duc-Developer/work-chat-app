b3ad0a8d102976935307be838877ed98b13a2116
DucJS <mrtranduc1994@outlook.com>
2020-08-25 21:30:22 +0700

    style with hover room

--- src/style/RoomCard.style.js
+++ src/style/RoomCard.style.js
@@ -4,16 +4,19 @@ export const RoomCardUseStyles = makeStyles({
     root: {
         display: "flex",
         justifyContent: "space-between",
-        margin: "5px 0 5px 0",
-        padding: "0 5px 0 5px",
+        padding: "5px 10px 5px 10px",
         alignItems: "center",
+        "&:hover": {
+            opacity: "0.8",
+            backgroundColor: "#dddddd"
+        }
     },
     avatar: {
         display: "flex",
         alignItems: "center",
         borderRadius: "50%",
-        maxWidth: "48px",
-        maxHeight: "48px",
+        maxWidth: "64px",
+        maxHeight: "64px",
         overflow: "hidden",
         flexGrow: "1"
     },
