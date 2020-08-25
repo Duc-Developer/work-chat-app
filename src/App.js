87fa58f5c9b3bd0676531c71dc2f2d3831e89d0c
DucJS <mrtranduc1994@outlook.com>
2020-08-25 09:48:39 +0700

    add path: redirect to login and register if have not user

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Auth from './auth';
 
 function App() {
+
+  const [isRedirect, setRedirect] = useState(true)
+
+  useEffect(() => {
+    let user = localStorage.getItem("userMail")
+    !user ? setRedirect(true) : setRedirect(false)
+  })
   return (
     <div className="App">
       <BrowserRouter>
-      <Switch>
-        <Route path="/auth">
-          <Auth />
-        </Route>
-      </Switch>
+        {isRedirect && <Redirect exact to="/auth" />}
+        <Switch>
+
+          <Route path="/auth">
+            <Auth />
+          </Route>
+        </Switch>
+
       </BrowserRouter>
     </div>
   );
--- src/auth/index.jsx
+++ src/auth/index.jsx
@@ -1,14 +1,51 @@
 import React from 'react'
-import { Switch, Route, useRouteMatch } from 'react-router-dom'
+import { Switch, Route, useRouteMatch, Link, Redirect, useHistory } from 'react-router-dom'
+import AppBar from '@material-ui/core/AppBar';
+import logoApp from '../images/work-chat-logo.png'
 import Register from './Register'
 import Login from './Login'
+import { Toolbar, Button, makeStyles, ButtonBase } from '@material-ui/core';
 
-const NotFound = () => <h1>NotFound...!</h1>
+const useStyles = makeStyles({
+    root: {
+        flexGrow: 1
+    },
+    home: {
+        flexGrow: 1
+    },
+    title: {
+        textAlign: "center"
+    }
+})
 
 export default function Auth() {
 
     const match = useRouteMatch()
-    return <div className="root-auth">
+    const history = useHistory()
+    const classes = useStyles()
+    return <div className={classes.root}>
+        <AppBar position="static">
+            <Toolbar>
+                <div className={classes.home}>
+                    <ButtonBase onClick={() => {history.push("/")}} >
+                        <img src={logoApp} alt="logo-app-bar" width="36px" />
+                    </ButtonBase>
+                </div>
+                <Button
+                    onClick={() => { history.push(`${match.url}/login`) }}
+                    color="inherit">
+                    Đăng nhập
+                </Button>
+                <Button
+                    onClick={() => { history.push(`${match.url}/register`) }}
+                    color="inherit">
+                    Đăng ký
+                </Button>
+            </Toolbar>
+        </AppBar>
+        <h1 className={classes.title}>
+            Bạn cần đăng nhập hoặc đăng ký để sử dụng ứng dụng
+        </h1>
         <Switch>
             <Route exact path={`${match.url}/register`}>
                 <Register />
@@ -16,8 +53,6 @@ export default function Auth() {
             <Route exact path={`${match.url}/login`}>
                 <Login />
             </Route>
-
-            <Route component={NotFound} />
         </Switch>
     </div>
 }
