7bb1e3484044f8333ba8f02efe6b98227d4a1061
DucJS <mrtranduc1994@outlook.com>
2020-08-26 11:55:43 +0700

    use history instead of Redirect to check userLogin

--- src/components/DashBoard/index.jsx
+++ src/components/DashBoard/index.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { DashBoardUseStyles as useStyles } from '../../style';
 import NavBar from '../NavBar';
 import SideBarLeft from '../SideBar/SideBarLeft';
 import MainBoardController from '../MainBoardController';
+import { useHistory } from 'react-router-dom';
 
 export default function DashBoard() {
 
-    const classes = useStyles()
+    const classes = useStyles();
+    const history = useHistory();
+
+    useEffect(() => {
+        let user = localStorage.getItem("userMail");
+        if(!user) {
+            history.push("/auth")
+        }
+    })
+
     return <div >
         <NavBar />
         <div className={classes.root}>
