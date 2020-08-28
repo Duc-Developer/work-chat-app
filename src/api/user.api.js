00044e6af17982583ad10e65ae30aa020ca3ae22
DucJS <mrtranduc1994@outlook.com>
2020-08-28 15:36:11 +0700

    fix bug api on updateUserProfile api

--- src/api/user.api.js
+++ src/api/user.api.js
@@ -2,19 +2,15 @@ import { database, storage } from '../firebase'
 import md5 from 'md5'
 
 export const checkUserData = (userId, typeCheck) => {
-    // let passwordRes = []
     return database.ref("users/" + userId)
-            .child(typeCheck)
-            .once("value")
-            .then(snapshot => {
-                // passwordRes.push(snapshot.val())
-                return snapshot.val()
-            })
-    // return passwordRes[0]
+        .child(typeCheck)
+        .once("value")
+        .then(snapshot => {
+            return snapshot.val()
+        })
 }
 
-export const updateUserProfile = (userId, data) => {
-    let imageFile = []
+export const updateUserProfile = async (userId, data) => {
     const {
         Fname,
         Lname,
@@ -28,34 +24,59 @@ export const updateUserProfile = (userId, data) => {
         introduce,
         phone,
         password,
-        townShip } = data
-    if(typeof(image) !== "string") {
-        storage.ref(`avatars/${userId}`)
-                .put(image)
-                .snapshot
-                .ref
-                .getDownloadURL()
-                .then(url => {
-                    imageFile.push(url)
-                })
+        townShip } = data;
+    if (typeof (image) !== "string") {
+        let uploadTask = storage
+            .ref()
+            .child(`avatars/avatarOf${userId}`).put(image);
+        uploadTask.on('state_changed',
+            (snapshot) => { },
+            (errors) => errors.message,
+            () => {
+                uploadTask
+                    .snapshot
+                    .ref
+                    .getDownloadURL()
+                    .then(url => {
+                        database
+                            .ref("users/" + userId)
+                            .set({
+                                Fname: Fname,
+                                Lname: Lname,
+                                address: address,
+                                city: city,
+                                company: company,
+                                country: country,
+                                email: email,
+                                gender: gender,
+                                introduce: introduce,
+                                phone: phone,
+                                password: md5(password),
+                                townShip: townShip,
+                                image: url
+                            })
+                    })
+            })
+        return;
     } else {
-        imageFile.push(image)
+        return database
+            .ref("users/" + userId)
+            .set({
+                Fname: Fname,
+                Lname: Lname,
+                address: address,
+                city: city,
+                company: company,
+                country: country,
+                email: email,
+                gender: gender,
+                introduce: introduce,
+                phone: phone,
+                password: md5(password),
+                townShip: townShip,
+                image: image
+            })
+            .then(snap => snap)
+            .catch(error => error)
     }
-    return database
-        .ref("users/" + userId)
-        .set({
-            Fname: Fname,
-            Lname: Lname,
-            address: address,
-            city: city,
-            company: company,
-            country: country,
-            email: email,
-            gender: gender,
-            introduce: introduce,
-            phone: phone,
-            password: md5(password),
-            townShip: townShip,
-            image: imageFile[0]
-        })
 }
