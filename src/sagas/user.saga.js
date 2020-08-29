91bc1388360d91a8b37389d96ac38b5a8be78a32
DucJS <mrtranduc1994@outlook.com>
2020-08-29 16:34:55 +0700

    md5 pass when user update

--- src/sagas/user.saga.js
+++ src/sagas/user.saga.js
@@ -1,15 +1,28 @@
 import { put, takeEvery } from 'redux-saga/effects';
-import { updateUserProfile } from '../api/user.api';
+import { updateUserProfile, checkUserData } from '../api/user.api';
 import { updateProfileSuccess, updateProfileFail } from '../actions/user.action';
 import { userConstants as userType } from '../constants'
+import md5 from 'md5';
 
 function* updateProfile(action) {
     let { payload } = action;
     let userId = sessionStorage.getItem("userId");
-    let error = yield updateUserProfile(userId, payload);
-    if(error) {
-        yield put(updateProfileFail(payload));
-        return;
+    let oldPass = yield checkUserData(userId, "password");
+    if(oldPass === payload.password) {
+        let error = yield updateUserProfile(userId, payload);  
+        if(error) {
+            yield put(updateProfileFail(payload));
+            return;
+        }  
+    }else {
+        let error = yield updateUserProfile(userId, {
+            ...payload,
+            password: md5(payload.password)
+        });
+        if(error) {
+            yield put(updateProfileFail(payload));
+            return;
+        }
     }
     yield put(updateProfileSuccess(payload))
 }
