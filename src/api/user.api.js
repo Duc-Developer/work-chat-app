65609f6e2fead9986b154480f09d2a15edfc0b76
DucJS <mrtranduc1994@outlook.com>
2020-09-02 20:34:41 +0700

    cut createRoomApi to room.api

--- src/api/user.api.js
+++ src/api/user.api.js
@@ -1,5 +1,4 @@
 import { database, storage } from '../firebase';
-import moment from 'moment'
 
 export const checkUserData = (userId, typeCheck) => {
     if (userId === "users") {
@@ -92,26 +91,3 @@ export const removeData = (path) => {
         .catch(error => error)
 }
 
-export const createRoomApi = (item1, item2) => {
-    const now = moment().format("hh:mm:ss DD-MM-YYYY")
-
-    let path = item1.userId < item2.userId
-        ? (item1.userId + item2.userId)
-        : item2.userId + item1.userId;
-    let data = {
-        users: [
-            { ...item1 },
-            { ...item2 }
-        ],
-        messages: {
-            "bot": {
-                time: now,
-                title: `Chúc mừng ${item1.Fname} và ${item2.Fname} đã trở thành bạn bè`
-            }
-        }
-    }
-    return database
-        .ref(`rooms/${path}`)
-        .update(data)
-        .catch(error => error);
-}
